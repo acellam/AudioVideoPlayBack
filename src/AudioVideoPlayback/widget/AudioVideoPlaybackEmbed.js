672d899bd6217ec2ff2256c1605b4b3fe22a3593
Acellam Guy <abiccel@yahoo.com>
2015-06-17 00:50:13 +0300

    Converted widget to AMD but with bugs to fix. unstable

---
+++ src/AudioVideoPlayback/widget/AudioVideoPlaybackEmbed.js
@@ -0,0 +1,204 @@
+/*jslint white:true, nomen: true, plusplus: true */
+/*global mx, define, require, browser, devel, console, document, jQuery */
+/*mendix */
+/*
+    AudioVideoPlayback
+    ========================
+
+    @file      : AudioVideoPlayback.js
+    @version   : 3.0.0
+    @author    : Acellam Guy
+    @date      : Tue, 16 Jun 2015 10:16:07 GMT
+    @copyright : Flock Of Birds
+    @license   : MIT
+
+    Documentation
+    ========================
+    Describe your widget here.
+*/
+
+// Required module list. Remove unnecessary modules, you can always get them back from the boilerplate.
+define([
+    'dojo/_base/declare', 'mxui/widget/_WidgetBase', 'dijit/_TemplatedMixin',
+    'mxui/dom', 'dojo/dom', 'dojo/query', 'dojo/dom-prop', 'dojo/dom-geometry', 'dojo/dom-class', 'dojo/dom-style', 'dojo/dom-construct', 'dojo/_base/array', 'dojo/_base/lang', 'dojo/text', 'dojo/html', 'dojo/_base/event',
+    'AudioVideoPlayback/lib/jquery-1.11.2.min', 'dojo/text!AudioVideoPlayback/widget/template/Embed.html','AudioVideoPlayback/lib/jQueryjPlayer/jqueryJplayerMin'
+], function (declare, _WidgetBase, _TemplatedMixin, dom, dojoDom, domQuery, domProp, domGeom, domClass, domStyle, domConstruct, dojoArray, lang, text, html, event, _jQuery, widgetTemplate) {
+    'use strict';
+
+    //var $ = jQuery.noConflict(true);
+    
+    // Declare widget's prototype.
+    return declare('AudioVideoPlayback.widget.AudioVideoPlaybackEmbed', [_WidgetBase, _TemplatedMixin], {
+
+        // _TemplatedMixin will create our dom node using this HTML template.
+        templateString: widgetTemplate,
+
+        // Parameters configured in the Modeler.
+        videoWidth: 640,
+        videoHeigth: 340,
+
+        // Internal variables. Non-primitives created in the prototype are shared between all widget instances.
+        _handles: null,
+        _contextObj: null,
+        _alertDiv: null,
+        
+         allowfullscreen: '', 
+
+        // dojo.declare.constructor is called to construct the widget instance. Implement to initialize non-primitive properties.
+        constructor: function () {
+            this._handles = [];
+        },
+        playback: function () {
+                var self = this; 
+                if(self.showFullScreenToggle)
+                    this.allowfullscreen ='allowfullscreen';
+
+               this.embed.innerHTML  ='<iframe frameborder="0" rel="0" width="'+self.videoWidth+'" height="'+self.videoHeigth+'" src="'+self.mediaURL+'" ' +this.allowfullscreen+'></iframe>';
+            },
+        // dijit._WidgetBase.postCreate is called after constructing the widget. Implement to do extra setup work.
+        postCreate: function () {
+            console.log(this.id + '.postCreate');
+            this._updateRendering();
+            this._setupEvents();
+            
+            this.playback();
+        
+            //this.actRendered();
+            
+        },
+
+        // mxui.widget._WidgetBase.update is called when context is changed or initialized. Implement to re-render and / or fetch data.
+        update: function (obj, callback) {
+            console.log(this.id + '.update');
+
+            this._contextObj = obj;
+            this._resetSubscriptions();
+            this._updateRendering();           
+
+            callback();
+        },
+
+        // mxui.widget._WidgetBase.enable is called when the widget should enable editing. Implement to enable editing if widget is input widget.
+        enable: function () {},
+
+        // mxui.widget._WidgetBase.enable is called when the widget should disable editing. Implement to disable editing if widget is input widget.
+        disable: function () {},
+
+        // mxui.widget._WidgetBase.resize is called when the page's layout is recalculated. Implement to do sizing calculations. Prefer using CSS instead.
+        resize: function (box) {},
+
+        // mxui.widget._WidgetBase.uninitialize is called when the widget is destroyed. Implement to do special tear-down work.
+        uninitialize: function () {
+            // Clean up listeners, helper objects, etc. There is no need to remove listeners added with this.connect / this.subscribe / this.own.
+        },
+
+        // We want to stop events on a mobile device
+        _stopBubblingEventOnMobile: function (e) {
+            if (typeof document.ontouchstart !== 'undefined') {
+                event.stop(e);
+            }
+        },
+
+        // Attach events to HTML dom elements
+        _setupEvents: function () {
+
+            
+
+        },
+
+        // Rerender the interface.
+        _updateRendering: function () {
+
+           
+
+            // Important to clear all validations!
+            this._clearValidations();
+        },
+
+        // Handle validations.
+        _handleValidation: function (_validations) {
+            this._clearValidations();
+
+            var _validation = _validations[0],
+                _message = _validation.getReasonByAttribute(this.backgroundColor);
+
+            if (this.readOnly) {
+                _validation.removeAttribute(this.backgroundColor);
+            } else {
+                if (_message) {
+                    this._addValidation(_message);
+                    _validation.removeAttribute(this.backgroundColor);
+                }
+            }
+        },
+
+        // Clear validations.
+        _clearValidations: function () {
+            domConstruct.destroy(this._alertdiv);
+            this._alertdiv = null;
+        },
+
+        // Show an error message.
+        _showError: function (message) {
+            if (this._alertDiv !== null) {
+                html.set(this._alertDiv, message);
+                return true;
+            }
+            this._alertDiv = domConstruct.create("div", {
+                'class': 'alert alert-danger',
+                'innerHTML': message
+            });
+            domConstruct.place(this.domNode, this._alertdiv);
+        },
+
+        // Add a validation.
+        _addValidation: function (message) {
+            this._showError(message);
+        },
+
+        // Reset subscriptions.
+        _resetSubscriptions: function () {
+            var _objectHandle = null,
+                _attrHandle = null,
+                _validationHandle = null;
+
+            // Release handles on previous object, if any.
+            if (this._handles) {
+                this._handles.forEach(function (handle, i) {
+                    mx.data.unsubscribe(handle);
+                });
+                this._handles = [];
+            }
+
+            // When a mendix object exists create subscribtions. 
+            if (this._contextObj) {
+
+                _objectHandle = this.subscribe({
+                    guid: this._contextObj.getGuid(),
+                    callback: lang.hitch(this, function (guid) {
+                        this._updateRendering();
+                    })
+                });
+
+                _attrHandle = this.subscribe({
+                    guid: this._contextObj.getGuid(),
+                    attr: this.backgroundColor,
+                    callback: lang.hitch(this, function (guid, attr, attrValue) {
+                        this._updateRendering();
+                    })
+                });
+
+                _validationHandle = this.subscribe({
+                    guid: this._contextObj.getGuid(),
+                    val: true,
+                    callback: lang.hitch(this, this._handleValidation)
+                });
+
+                this._handles = [_objectHandle, _attrHandle, _validationHandle];
+            }
+        }
+    });
+});
+require(['AudioVideoPlayback/widget/AudioVideoPlaybackEmbed'], function () {
+    'use strict';
+});
